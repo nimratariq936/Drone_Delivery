d56f1c201d74c2957d422f892adf2776a4387b33
nimratariq936 <nimratariq936@gmail.com>
2026-04-29 23:37:13 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Drone Delivery System - Version 1.0" << endl;
+    cout << "Initial flight algorithm loaded." << endl;
+    cout << "Package tracking module ready." << endl;
+    return 0;
+}
